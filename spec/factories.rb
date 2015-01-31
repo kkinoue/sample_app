5fb042686e7ffcac00f9e73b38e02688d1afeda7
h.inoue <h.inoue@technoplan-inc.co.jp>
2015-01-31 23:43:39 +0900

    Finish Chapters 9, Finish user edit, update, index, and destroy actions.

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,14 @@
 FactoryGirl.define do
+
   factory :user do
-    name 'hiroshi inoue'
-    email 'inoue@example.com'
-    password 'pass1234'
-    password_confirmation 'pass1234'
+    sequence(:name) { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com" }
+    password 'foobar'
+    password_confirmation 'foobar'
+
+    factory :admin do
+      admin true
+    end
   end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,8 +15,10 @@ describe User do
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:admin) }
 
   it { should be_valid }
+  it { should_not be_admin }
 
   describe "when name is not present" do
     before { @user.name = '' }
@@ -45,7 +47,7 @@ describe User do
 
   describe "when email format is valid" do
     it "should be valid" do
-      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn user@a2.d.com]
+      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn user@a2.d.com a_2@a.com]
       addresses.each do |valid_email|
         @user.email = valid_email
         expect(@user).to be_valid
@@ -102,4 +104,14 @@ describe User do
     before { @user.save }
     its(:remember_token) { should_not be_blank }
   end
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
+
 end
